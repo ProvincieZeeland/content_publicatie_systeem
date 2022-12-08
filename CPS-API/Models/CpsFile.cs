4a4a3f2781a9d6a7bcd9f0e8f5115d4ba590ace4
Anne Braamburg <a.braamburg@i-experts.nl>
2022-12-08 15:20:35 +0100

    JSON ByteArrayConverter

--- CPS-API/Models/CpsFile.cs
+++ CPS-API/Models/CpsFile.cs
@@ -1,7 +1,11 @@
-﻿namespace CPS_API.Models
+﻿using CPS_API.Helpers;
+using System.Text.Json.Serialization;
+
+namespace CPS_API.Models
 {
     public class CpsFile
     {
+        [JsonConverter(typeof(ByteArrayConverter))]
         public byte[] Content { get; set; }
 
         public FileInformation Metadata { get; set; }
